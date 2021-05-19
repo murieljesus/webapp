867bb56ad4bc7cf662d259f5fef6087c58143248
murieljesus <jmuriel72@gmail.com>
2021-05-19 02:49:29 -0300

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -37,8 +39,8 @@ import { ContactComponent } from './contact/contact.component';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
-    
+    MatButtonModule,
+    AppRoutingModule    
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
